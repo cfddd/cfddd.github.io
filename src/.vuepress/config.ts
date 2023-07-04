52f006532a36e1b1f1fbd7214f3e43d51678ab53
cfd <2909047212@qq.com>
2023-07-04 11:15:28 +0800

    modify the link

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/cfddd.github.io/",
 
   lang: "zh-CN",
   title: "俄罗斯刺沙蓬",
